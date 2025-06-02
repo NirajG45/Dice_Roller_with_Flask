d6066f4fcf532c4f14c3a5da1fd325cef95cd369
Niraj Kumar <nirajkumar9945111@gmail.com>
2025-06-02 23:42:47 +0530

    Updating my backend code

--- Dice.py
+++ Dice.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, session
 import random
+from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = 'dice-secret-key'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/roll')
-def roll():
-    number = random.randint(1, 6)
-    return jsonify({'number': number})
+@app.route('/roll', methods=['GET'])
+def roll_dice():
+    dice_value = random.randint(1, 6)
+    roll_time = datetime.now().strftime("%H:%M:%S")
+
+    if 'history' not in session:
+        session['history'] = []
+
+    session['history'].insert(0, {'value': dice_value, 'time': roll_time})
+    session['history'] = session['history'][:5]  # keep last 5 rolls
+
+    return jsonify({
+        'value': dice_value,
+        'history': session['history']
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
